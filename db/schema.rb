aedc3c956bfa18f67c7ee2abd584e7c573033cca
Daniel Mobayyen <daniel.mobayyen@kellogg.northwestern.edu>
2023-04-27 00:41:30 +0000

    Commit_v2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_002550) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_003712) do
   create_table "activities", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -21,6 +21,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_002550) do
     t.string "city"
     t.string "state"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.integer "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
